2486ac4a4aa91cef861579c6d3859da962f14d37
Mohammad Nakhjiri <mohammaddev02@gmail.com>
2022-05-31 21:35:17 +0430

    finished category

--- lib/pages/CategoryPage.dart
+++ lib/pages/CategoryPage.dart
@@ -11,32 +11,139 @@ class CategoryPage extends StatelessWidget {
         Column(
           children: [
             Container(
-                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height/10),
+                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height/20),
                 child: Text("کالای دیجیتال",
                   style: TextStyle(
                     fontFamily: "vazirbold",
                     fontSize: 20,
                   ),
                 )),
-            Row(
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/20),
-                      alignment: Alignment.center,
-                      child: Image.asset("lib/img/mobile.png",
-                        width: MediaQuery.of(context).size.width/3,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            )
+            SizedBox(
+              height: MediaQuery.of(context).size.height/(3),
+              child: ListView(
+                padding: EdgeInsets.all(20),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  categoryItem("lib/img/mobile.png" , "موبایل"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/laptop.png" , "لپ تاپ"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/camera.png" , "دوربین"),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height/20),
+                child: Text("پوشاک",
+                  style: TextStyle(
+                    fontFamily: "vazirbold",
+                    fontSize: 20,
+                  ),
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.height/(3),
+              child: ListView(
+                padding: EdgeInsets.all(20),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  categoryItem("lib/img/women.png" , "زنانه"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/men.png" , "مردانه"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/children.png" , "بچگانه"),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height/20),
+                child: Text("کتاب و لوازم تحریر",
+                  style: TextStyle(
+                    fontFamily: "vazirbold",
+                    fontSize: 20,
+                  ),
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.height/(3),
+              child: ListView(
+                padding: EdgeInsets.all(20),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  categoryItem("lib/img/book.png" , "کتاب"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/stationary.png" , "لوازم تحریر"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/music.png" , "موسیقی"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/handicrafts.png" , "صنایع دستی"),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height/20),
+                child: Text("ورزش و سفر",
+                  style: TextStyle(
+                    fontFamily: "vazirbold",
+                    fontSize: 20,
+                  ),
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.height/(3),
+              child: ListView(
+                padding: EdgeInsets.all(20),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  categoryItem("lib/img/sport-clothes.png" , "پوشاک ورزشی"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/sporteq.png" , "لوازم ورزشی"),
+                  SizedBox(width: 20,),
+                  categoryItem("lib/img/camping.png" , "لوازم سفر و کمپینگ"),
+                ],
+              ),
+            ),
           ],
         )
       ],
+    );
+  }
+}
+
+class categoryItem extends StatelessWidget {
+  final imgPath;
+  final imgText;
+  categoryItem(this.imgPath, this.imgText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/20),
+      alignment: Alignment.center,
+      child: ElevatedButton(
+        onPressed: (){},
+        child: Column(
+          children: [
+            Image.asset(imgPath,
+              height: MediaQuery.of(context).size.height/8,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height/30,
+            ),
+            Text(imgText,
+            style: TextStyle(
+              fontFamily: "vazirbold",
+              color: Colors.black,
+            ),
+            )
+          ],
+        ),
+        style:
+        ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(10),
+          primary: Color(0xFFFFFFFF),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          textStyle: TextStyle(color: Colors.black),
+        ),
+      ),
     );
   }
 }
