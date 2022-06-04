c2c59b3122e2b79806288cc4f693aafb1577645b
Mohammad Nakhjiri <mohammaddev02@gmail.com>
2022-06-04 15:47:47 +0430

    buy added

--- lib/pages/Buy.dart
+++ lib/pages/Buy.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:namakala/pages/Buy.dart';
 import 'package:namakala/widgets/CartSection.dart';
 import 'package:namakala/widgets/NavigationBar.dart';
 import 'package:persian_number_utility/persian_number_utility.dart';
@@ -7,44 +6,25 @@ import 'package:persian_number_utility/persian_number_utility.dart';
 import '../widgets/RadioGroup.dart';
 import '../widgets/SearchContainer.dart';
 
-class CartPage extends StatefulWidget {
+class Buy extends StatefulWidget {
   @override
-  State<CartPage> createState() => _CartPageState();
+  State<Buy> createState() => _BuyState();
 
-  const CartPage({Key? key}) : super(key: key);
+  const Buy({Key? key}) : super(key: key);
 }
 
-class _CartPageState extends State<CartPage> {
+class _BuyState extends State<Buy> {
   @override
   void initState() {
     super.initState();
   }
-  var icon = Icons.favorite_border;
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                CartSection(),
-                SizedBox(height: 5,),
-                CartSection.arg("گوشی  samsung", "lib/img/products/samsung.png", "30,000,000", "4"),
-                SizedBox(height: 5,),
-                CartSection.arg("گوشی  شیاومی", "lib/img/products/mi.png", "20,000,000", "4.5"),
-                SizedBox(height: 5,),
-                CartSection.arg("گوشی  samsung", "lib/img/products/samsung2.png", "28,000,000", "4.7"),
-                SizedBox(height: 5,),
-              ],
-            ),
-          ),
-        ],
-      ),
       appBar: AppBar(
         centerTitle: true,
-        title: Text("سبد خرید" , style: TextStyle(color: Colors.black , fontWeight: FontWeight.bold),),
+        title: Text("تکمیل خرید" , style: TextStyle(color: Colors.black , fontWeight: FontWeight.bold),),
         backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
         elevation: 0.0,
         leading: IconButton(
@@ -54,7 +34,17 @@ class _CartPageState extends State<CartPage> {
           },
         ),
       ),
-
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: const [
+                Center(child: Text("برای تکمیل خرید ابتدا باید وارد حساب کاربری خود شوید"))
+              ],
+            ),
+          )
+        ],
+      ),
 
       bottomNavigationBar: SizedBox(
         height: MediaQuery.of(context).size.height/10,
@@ -64,26 +54,16 @@ class _CartPageState extends State<CartPage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Center(child: Text(   "30,000,000 تومان".toPersianDigit() , style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20
-              ),),
-              ),
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Buy()));
-
-              }, child: Container(
-                child: Text("ادامه فرایند خرید" , style: TextStyle(color: Colors.white),),
+              ElevatedButton(onPressed: (){}, child: Container(
+                child: Text("تکمیل خرید" , style: TextStyle(color: Colors.white),),
               ),
                   style: ButtonStyle(
                       padding: MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.only(left: 30 , right: 30)),
+                          EdgeInsets.only(left: 70 , right: 70 , top : 15 , bottom : 15)),
                       backgroundColor:  MaterialStateProperty.all(Colors.black),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            borderRadius: BorderRadius.circular(10),
                           )
                       )
                   )
